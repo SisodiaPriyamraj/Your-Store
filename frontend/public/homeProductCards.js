5dbaa60096c0584ccd0d5ba1bf802e37f9c4f3a5
SisodiaPriyamraj <priyamrajsinghsisodia@gmail.com>
2024-07-03 14:53:14 +0530

    Created products.json and completed the footer

---
+++ frontend/public/homeProductCards.js
@@ -0,0 +1,14 @@
+const productConatainer = document.querySelector(".productContainer");
+const productTemplate = document.querySelector(".productTemplate");
+
+
+
+export const showProductContainer = (products) => {
+    if(!products){
+        return false;
+    }
+
+    product.foreach((curElem)=>{
+        const{brand, category, description, id, image, name, price, stock}=curProd;
+    })
+}
